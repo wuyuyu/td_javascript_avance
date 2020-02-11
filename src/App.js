390cbe73ea8747e4c914dd1a9ef70b2dd7d1e83e
Tania <tania.rojas@edu.itescia.fr>
2020-02-11 14:59:34 +0100

    correction

--- src/App.js
+++ src/App.js
@@ -1,53 +1,70 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Accueil from './Accueil';
 import Config from './Config';
 import About from './About';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from "react-router-dom";
 
-class App extends Component{
-  render(){
-    return(
-      <Router>
-      <Accueil/>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Accueil</Link>
-            </li>
-            <li>
-              <Link to="/about">Config</Link>
-            </li>
-            <li>
-              <Link to="/users">About</Link>
-            </li>
-          </ul>
-        </nav>
+class App extends Component {
+    render() {
+        return ( <
+            Router >
+            <
+            Accueil / >
+            <
+            div >
+            <
+            nav >
+            <
+            ul >
+            <
+            li >
+            <
+            Link to = "/" > Accueil < /Link> <
+            /li> <
+            li >
+            <
+            Link to = "/users" > Config < /Link> <
+            /li> <
+            li >
+            <
+            Link to = "/about" > About < /Link> <
+            /li> <
+            /ul> <
+            /nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Config />
-          </Route>
-          <Route path="/">
-            <Accueil />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-    );
-  }
+            {
+                /* A <Switch> looks through its children <Route>s and
+                            renders the first one that matches the current URL. */
+            } <
+            Switch >
+            <
+            Route path = "/about" >
+            <
+            About / >
+            <
+            /Route> <
+            Route path = "/users" >
+            <
+            Config / >
+            <
+            /Route> <
+            Route path = "/" >
+            <
+            Accueil / >
+            <
+            /Route> <
+            /Switch> <
+            /div> <
+            /Router>
+        );
+    }
 }
 
 
-  export default App;
+export default App;
